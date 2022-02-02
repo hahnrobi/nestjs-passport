37082b4905a7c91611238ef6d2383a4b2b1f60cd
Hahn Robi <robitwo.r2@gmail.com>
2022-02-02 16:06:40 +0100

    JWT almost done. Constant from env is missing.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './local-auth.guard';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    ConfigModule.forRoot({}),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    })
+  ],
+  providers: [AuthService, LocalStrategy, LocalAuthGuard],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      email: 'robi',
+      password: '1234',
+    },
+    {
+      userId: 2,
+      email: 'test',
+      password: '4567',
+    },
+  ];
+
+  async findOne(email: string): Promise<User | undefined> {
+	  Logger.log(email);
+    return this.users.find(user => user.email === email);
+  }
+}
