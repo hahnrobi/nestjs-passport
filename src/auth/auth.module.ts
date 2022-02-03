97914ad69d973ccc2b5cb9372164278b3d4bb0ca
Hahn Robi <robitwo.r2@gmail.com>
2022-02-03 11:15:53 +0100

    Login works.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -12,13 +13,12 @@ import { jwtConstants } from './constants';
   imports: [
     UsersModule,
     PassportModule,
-    ConfigModule.forRoot({}),
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
-    })
+    }),
   ],
-  providers: [AuthService, LocalStrategy, LocalAuthGuard],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
